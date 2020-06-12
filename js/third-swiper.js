78a701c0b642e5d2e7c749930d3676c6a3db2927
Karyna04 <karynalvova@gmail.com>
2020-06-12 14:47:56 +0200

    name

---
+++ js/third-swiper.js
@@ -0,0 +1,8 @@
+// To use lazy loading, set a data-lazy attribute
+// on your img tags and leave off the src
+
+$(".your-class").slick({
+  lazyLoad: "ondemand",
+  slidesToShow: 3,
+  slidesToScroll: 1
+});
